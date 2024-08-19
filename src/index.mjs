ee1069d7f7162a49344d018d4d142231542989c6
Drin Bratotini <drin.bratotini@gmail.com>
2024-08-19 12:42:56 -0400

    learning sessions

--- src/index.mjs
+++ src/index.mjs
@@ -3,8 +3,15 @@ import express from "express";
 import { loggingMiddleware } from "./utils/middleware.mjs";
 import cookieParser from "cookie-parser";
 import session from "express-session";
+import {
+  query,
+  validationResult,
+  checkSchema,
+  matchedData,
+} from "express-validator";
 
 import routes from "./routes/index.mjs";
+import { mockUsers } from "./utils/constants.mjs";
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
@@ -29,6 +36,48 @@ app.get("/", (req, res) => {
   req.session.visited = true;
   res.cookie("value", "something", { maxAge: 60000 * 60 });
   res.status(201).send({ msg: "hello" });
+});
+
+app.post("/api/auth", (req, res) => {
+  const {
+    body: { username, password },
+  } = req;
+  const findUser = mockUsers.find((u) => u.username === username);
+  if (!findUser || findUser.password !== password)
+    return res.status(401).send({ error: "unauthenticated" });
+  // attach the found user to the session object
+  req.session.user = findUser;
+  return res.status(200).send(findUser);
+});
+
+app.get("/api/auth/status", (req, res) => {
+  req.sessionStore.get(req.sessionID, (err, session) => {
+    console.log(session);
+    console.log(req.sessionStore);
+    console.log(req.session.cart);
+  });
+  return req.session.user
+    ? res.status(200).send(req.session.user)
+    : res.status(401).send({ error: "not authenticated" });
+});
+
+app.post("/api/cart", (req, res) => {
+  if (!req.session.user) return res.sendStatus(401);
+  //renaming to cartItem
+  const { body: cartItem } = req;
+  const { cart } = req.session;
+
+  if (cart) {
+    cart.push(cartItem);
+  } else {
+    req.session.cart = [cartItem];
+  }
+  return res.status(201).send(cartItem);
+});
+
+app.get("/api/cart", (req, res) => {
+  if (!req.session.user) return res.sendStatus(401);
+  res.send(req.session.cart ?? []);
 });
 
 app.listen(PORT, () => {
--- src/utils/constants.mjs
+++ src/utils/constants.mjs
@@ -1,8 +1,9 @@
-const mockUsers = [
-  { id: 1, username: "jack", name: "Jack" },
-  { id: 2, username: "patrick", name: "Patrick" },
-  { id: 3, username: "enzo", name: "Enzo" },
-  { id: 4, username: "henry", name: "Henry" },
+export const mockUsers = [
+  { id: 1, username: "anson", displayName: "Anson", password: "hello123" },
+  { id: 2, username: "jack", displayName: "Jack", password: "hello124" },
+  { id: 3, username: "adam", displayName: "Adam", password: "hellohello" },
+  { id: 4, username: "tina", displayName: "Tina", password: "test123" },
+  { id: 5, username: "jason", displayName: "Jason", password: "hello123" },
+  { id: 6, username: "henry", displayName: "Henry", password: "hello123" },
+  { id: 7, username: "marilyn", displayName: "Marilyn", password: "hello123" },
 ];
-
-export { mockUsers };
